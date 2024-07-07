c038c3a06bb07a515587eb500941fb7af21c9a43
Jorge Eduardo Jiménez Ramírez <Balmund@Thaumazo.lan>
2024-07-07 18:52:09 -0400

    First Commit

---
+++ Starlit_One/src/main/java/com/Ironhack/Starlit_One/StarlitOneApplication.java
@@ -0,0 +1,13 @@
+package com.Ironhack.Starlit_One;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlitOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarlitOneApplication.class, args);
+	}
+
+}
---
+++ Starlit_One/src/test/java/com/Ironhack/Starlit_One/StarlitOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ironhack.Starlit_One;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarlitOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
